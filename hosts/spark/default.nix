038bac98af63969aafb11357a020d536c7a4cb5a
Petter Søland <petter.soland@gmail.com>
2026-04-10 12:53:20 +0200

    Updated code-server

--- hosts/spark/default.nix
+++ hosts/spark/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
 
@@ -6,10 +6,23 @@
     ../../modules/common.nix
   ];
 
-  services.code-server = {
-    enable = true;
-    bindAddr = "127.0.0.1:8080";
-    auth = "none";
+  home.packages = [ pkgs.code-server ];
+
+  systemd.user.services.code-server = {
+    Unit = {
+      Description = "code-server";
+      After = [ "network.target" ];
+    };
+
+    Service = {
+      ExecStart = "${pkgs.code-server}/bin/code-server --bind-addr 127.0.0.1:8080 --auth none";
+      Restart = "on-failure";
+      RestartSec = 2;
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
   };
 
   home.stateVersion = "25.11";

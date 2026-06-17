6b977512bdd48c03cfcef45d0f3e54e7b8be3544
Petter Søland <petter.soland@gmail.com>
2026-06-17 16:53:45 +0200

    Added settings for dock

--- modules/darwin.nix
+++ modules/darwin.nix
@@ -38,6 +38,19 @@
       "show-recents" = false;
       "wvous-bl-corner" = 4;
       "wvous-br-corner" = 12;
+
+      persistent-apps = [
+        "/System/Applications/Calendar.app"
+        "/Applications/Slack.app"
+        "/Applications/Spotify.app"
+        "/Applications/Ghostty.app"
+        "/Applications/Obsidian.app"
+      ];
+
+      persistent-others = [
+        "/Applications"
+        "/Users/pettersoland/Downloads"
+      ];
     };
 
     trackpad = {

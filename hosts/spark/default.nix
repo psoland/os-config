63f013026e6dc83cb571004b21fff9c44a74f930
Petter Søland <petter.soland@gmail.com>
2026-07-01 17:39:09 +0200

    Added cloudflared settings to systemd

--- hosts/spark/default.nix
+++ hosts/spark/default.nix
@@ -119,6 +119,23 @@
     };
   };
 
+  systemd.user.services.cloudflared = {
+    Unit = {
+      Description = "Cloudflare Tunnel";
+      After = [ "network.target" ];
+    };
+
+    Service = {
+      ExecStart = "${pkgs.cloudflared}/bin/cloudflared tunnel --no-autoupdate run --token-file %h/.config/cloudflared/token";
+      Restart = "always";
+      RestartSec = 5;
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
+
   home.stateVersion = "25.11";
 
 }

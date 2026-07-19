6283125bfc4bc82789781bb534863710ffdbd4b8
Petter Søland <petter.soland@gmail.com>
2026-07-19 18:45:48 +0200

    Added shortcut for blink toggle

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,13 @@
 -- Add any additional keymaps here
 
 vim.keymap.del("n", "<leader><tab><tab>")
+
+vim.keymap.set("n", "<leader>uB", function()
+  if vim.b.completion == false then
+    vim.b.completion = nil
+  else
+    vim.b.completion = false
+  end
+  require("blink.cmp").hide()
+  vim.notify("Completion " .. (vim.b.completion == false and "disabled" or "enabled") .. " for this buffer")
+end, { desc = "Toggle Blink Completion" })

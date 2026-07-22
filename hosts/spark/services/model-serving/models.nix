2ca4b0cd82ab67a0f6a6f8803a80b9ee551f52e5
Petter Søland <petter.soland@gmail.com>
2026-07-22 21:37:25 +0200

    Updated default vllm

--- hosts/spark/services/model-serving/models.nix
+++ hosts/spark/services/model-serving/models.nix
@@ -2,7 +2,7 @@
 # ~/.config/vllm/models.json and are reconciled with `vllmctl update`.
 {
   basePort = 8015;
-  image = "vllm/vllm-openai:v0.24.0";
+  image = "vllm/vllm-openai:v0.25.1";
   gpus = "all";
   gpuMemoryUtilization = 0.60;
   maxModelLen = 8192;

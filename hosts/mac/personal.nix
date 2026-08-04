623619faed91103201b29f2723e504a5b51dc79b
Petter Søland <petter.soland@gmail.com>
2026-08-04 09:49:43 +0200

    Added tailscale force reauth to mac

--- hosts/mac/personal.nix
+++ hosts/mac/personal.nix
@@ -2,6 +2,10 @@
 
 {
 
+  programs.zsh.shellAliases = {
+    ts-reauth = "/Applications/Tailscale.app/Contents/MacOS/Tailscale up --force-reauth";
+  };
+
   imports = [
     ../../profiles/home/common.nix
     ../../modules/home/programs/ghostty.nix
